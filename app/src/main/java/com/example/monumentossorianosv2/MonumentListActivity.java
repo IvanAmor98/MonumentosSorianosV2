5f42b1de8820c2bd5b591c9565e83c84b47f0767
Ivan <ivan.amor@innovadeluxe.com>
2021-12-07 14:25:49 +0100

    Fixed typos

--- app/src/main/java/com/example/monumentossorianosv2/MonumentListActivity.java
+++ app/src/main/java/com/example/monumentossorianosv2/MonumentListActivity.java
@@ -2,7 +2,6 @@ package com.example.monumentossorianosv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,9 +11,9 @@ import java.util.ArrayList;
 
 public class MonumentListActivity extends AppCompatActivity {
 
-    ListView lvMonumentos;
+    ListView lvMonuments;
     ListAdapter adapter;
-    ArrayList<MonumentDTO> monumentos;
+    ArrayList<MonumentDTO> monuments;
     MonumentDAO monumentDAO = new MonumentDAO(this);
 
     @Override
@@ -23,16 +22,16 @@ public class MonumentListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_monument_list);
 
         monumentDAO = new MonumentDAO(this);
-        monumentos = monumentDAO.listMonuments();
+        monuments = monumentDAO.listMonuments();
 
-        lvMonumentos = findViewById(R.id.monumentList);
-        adapter = new ListAdapter(this, R.layout.monument_list_row, monumentos);
-        lvMonumentos.setAdapter(adapter);
+        lvMonuments = findViewById(R.id.monumentList);
+        adapter = new ListAdapter(this, R.layout.monument_list_row, monuments);
+        lvMonuments.setAdapter(adapter);
 
-        lvMonumentos.setOnItemClickListener((parent, view, position, id) -> {
+        lvMonuments.setOnItemClickListener((parent, view, position, id) -> {
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            String address = monumentos.get(position).getAddress();
-            address.replace(' ', '+');
+            String address = monuments.get(position).getAddress();
+            address = address.replace(' ', '+');
             Uri.parse("google.navigation:q=" + address);
             startActivity(intent);
         });
